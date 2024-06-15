03a42350208f4c007c76e2aa669c3c93757fa169
Ryan Montoyo <ryanmontoyo2003@gmail.com>
2024-06-15 18:06:30 -0400

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Ryan Montoyo on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
